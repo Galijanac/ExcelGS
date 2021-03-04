b053564f938509dd5afd096da0cbfd3fd7b3fdc1
Nikola Etinski <netinski@gamma-soft.com>
2021-03-04 17:25:28 +0100

    update

--- ExcelGS/ExcelGS/Program.cs
+++ ExcelGS/ExcelGS/Program.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Configuration;
+using System.Reflection;
 
 namespace ExcelGS
 {
@@ -10,6 +24,12 @@ namespace ExcelGS
     {
         static void Main(string[] args)
         {
+            string path = Console.ReadLine();
+            int rowsRead = 1;
+
+            var xlApp = new Excel.Application();
+            var xlWorkBookRead = xlApp.Workbooks.Open(path, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+
         }
     }
 }
